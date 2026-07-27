a79443d5b4508d06ad9f01792eb5a28bca63b2f6
shivampatelaiml <shivampatelaiml@gmail.com>
2026-07-27 13:18:07 +0530

    Day 07 - Learn Python While Loop

---
+++ Day07/while_loop.py
@@ -0,0 +1,32 @@
+"""
+Day 07 - While Loop
+Author: Shivam Patel
+Topic: Python While Loop
+"""
+
+# Print numbers from 1 to 10
+print("Numbers from 1 to 10:")
+count = 1
+
+while count <= 10:
+    print(count)
+    count += 1
+
+# Print even numbers from 2 to 20
+print("\nEven Numbers:")
+num = 2
+
+while num <= 20:
+    print(num)
+    num += 2
+
+# Countdown Timer
+print("\nCountdown:")
+
+timer = 5
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("🚀 Time's Up!")
